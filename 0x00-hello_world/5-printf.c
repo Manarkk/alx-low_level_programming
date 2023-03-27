0fbc7dd5318b60268e98d23eb3d3c76125a1f78c
phoenix <manar.karabela00@gmail.com>
2023-03-27 11:44:45 +0000

    printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ *
+ * main - print text
+ * str = Hello World
+ * return : Always 0 (Sucess)
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,);
+	return 0;
+}

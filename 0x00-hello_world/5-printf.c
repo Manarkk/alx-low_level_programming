02118d6a8610fa249e0c8c3812ce56d78dd14478
phoenix <manar.karabela00@gmail.com>
2023-03-27 11:50:20 +0000

    printf in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 /**
  *
- * main - print text
+ * main - prints text
+ *
  * str = Hello World
+ *
  * return : Always 0 (Sucess)
  */
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

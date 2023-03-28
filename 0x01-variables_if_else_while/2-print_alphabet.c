d423e4335ca82badfd223359cc9bd3ba8684ca4a
phoenix <manar.karabela00@gmail.com>
2023-03-28 17:31:06 +0000

    if statment

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int n;
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return 0;
+}	

343885674c41a36c73fc1ea6120b03bab668a89c
phoenix <manar.karabela00@gmail.com>
2023-03-28 18:22:38 +0000

    print alphabet besides 2 letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++);
-	{
-		if (ch != 'e' && ch != 'q');
-		putchar(ch);
-	}
-	putchar('\n');
+char ch;
 
-	return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	if (ch != 'e' && ch != 'q')
+	putchar(alphabet);
+}
+putchar('\n');
+
+return (0);
 }
